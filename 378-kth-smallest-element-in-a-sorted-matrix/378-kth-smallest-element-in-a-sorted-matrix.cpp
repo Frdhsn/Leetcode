cfe56137b4b83b2977d0a4a8fc9f81cff87ca38c
Md. Farhad Hossain <iamforu299@gmail.com>
2022-08-03 01:39:07 +0600

    Time: 48 ms (63.09%), Space: 14.2 MB (45.52%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -3,12 +3,20 @@ public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         int cnt=1;
         vector<int>v;
+        priority_queue<int> pq;
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
-                v.push_back(matrix[i][j]);
+                if(pq.size()<k){
+                    pq.push(matrix[i][j]);
+                }
+                else{ // pq.size() == k
+                    if(pq.top()>matrix[i][j]){
+                        pq.pop();
+                        pq.push(matrix[i][j]);
+                    }
+                }
             }
         }
-        sort(v.begin(),v.end());
-        return v[k-1];
+        return pq.top();
     }
 };
