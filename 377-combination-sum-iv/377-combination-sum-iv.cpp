1c06db8e741782362444061b0365542706e93c87
Md. Farhad Hossain <iamforu299@gmail.com>
2022-08-05 08:34:11 +0600

    Time: 3 ms (62.90%), Space: 7.6 MB (7.58%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -3,11 +3,11 @@ public:
     using ll = long long int;
     int dp[205][1005];
     int go(int idx,vector<int>& nums, int target){
-        if(idx == nums.size())
+        if(target < 0)
             return 0;
         if(target == 0)
             return 1;
-        if(target < 0)
+        if(idx == nums.size())
             return 0;
         if(dp[idx][target]!=-1)
             return dp[idx][target];
