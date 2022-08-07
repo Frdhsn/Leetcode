7b7acccaf51651298772ae1a114cf81eac4eec83
Md. Farhad Hossain <iamforu299@gmail.com>
2022-08-07 06:38:16 +0600

    Time: 14 ms (78.35%), Space: 8.4 MB (66.36%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -2,15 +2,20 @@ class Solution {
 public:
     static const int MAX=2e4+7,MOD = 1e9+7;
     int dp[MAX][5];
-    unordered_map<char,int> mp;
     
     int go(int idx,char prev){
         if(idx == 1){
             
             return 1;
         }
-        if(dp[idx][mp[prev]]!=-1)
-            return dp[idx][mp[prev]];
+        int id;
+        if(prev=='a')id=0;
+        if(prev=='e')id=1;
+        if(prev=='i')id=2;
+        if(prev=='o')id=3;
+        if(prev=='u')id=4;
+        if(dp[idx][id]!=-1)
+            return dp[idx][id];
         int ret=0;
         if(prev=='a'){
             ret=(ret%MOD + go(idx-1,'e'))%MOD;
@@ -32,18 +37,13 @@ public:
         if(prev=='u'){
             ret=(ret%MOD + go(idx-1,'a'))%MOD;
         }
-        return dp[idx][mp[prev]] = ret%MOD;
+        return dp[idx][id] = ret%MOD;
     }
     int countVowelPermutation(int n) {
         memset(dp,-1,sizeof dp);
         
         int ret=0;
         string vowel="aeiou";
-        mp['a']=0;
-        mp['e']=1;
-        mp['i']=2;
-        mp['o']=3;
-        mp['u']=4;
         for(auto x:vowel)
         {
             
