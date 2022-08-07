b4a86c20230e2bf833555a06f194f60bebdede5b
Md. Farhad Hossain <iamforu299@gmail.com>
2022-08-07 10:46:31 +0600

    Time: 1142 ms (11.11%), Space: 183.1 MB (11.11%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     vector<int> g[100005];
     int res;
-    void dfs(int u,int p,vector<int>& bad){
-        if(bad[u])return;
+    void dfs(int u,int p,vector<int>& bad,vector<int>& vs){
+        if(bad[u] or vs[u])return;
+        vs[u]=1;
         res++;
         for(auto v:g[u]){
             if(v!=p)
-                dfs(v,u,bad);
+                dfs(v,u,bad,vs);
         }
     }
     int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
@@ -16,11 +17,11 @@ public:
             g[x[0]].push_back(x[1]);
             g[x[1]].push_back(x[0]);
         }
-        vector<int> bad(n+7, 0);
+        vector<int> bad(n+7, 0),vs(n+7, 0);
         for(auto x:restricted)
             bad[x]=1;
         res=0;
-        dfs(0,0,bad);
+        dfs(0,0,bad,vs);
         return res;
     }
 };
