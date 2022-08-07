32a8442c8ed1b95b6b7deae28ae30171db0de6c8
Md. Farhad Hossain <iamforu299@gmail.com>
2022-08-07 10:45:04 +0600

    Time: 214 ms (77.78%), Space: 87.3 MB (22.22%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,35 +1,21 @@
-
 class Solution {
-    public:
-    int n;
-    int dp[100100];
-    bool ret[100100];
-    vector<int>num;
-
-    bool solve(int i){
-        if(i>=n) return true;
-        if(dp[i]!=-1) return ret[i];
-
-        bool ans = false;
-
-        if(i+1<n && num[i+1]==num[i]){
-            if(solve(i+2)==true) ans = true;
-        }
-        if(i+2<n && num[i]==num[i+1] && num[i]==num[i+2]){
-            if(solve(i+3)==true) ans = true;
-        }
-        if(i+2<n && num[i]+1==num[i+1] && num[i]+2==num[i+2]){
-            if(solve(i+3)==true) ans = true;
-        }
-        dp[i] = 1;
-        return ret[i] = ans;
-    }
-
+public:
+    //bool conq(int l,int r,vector<int>& nums)
     bool validPartition(vector<int>& nums) {
-        n = nums.size();
-        memset(dp, -1, sizeof(dp));
-        num = nums;
-
-        return solve(0);
+        int n = nums.size();
+        
+        vector<int> dp(n+7);
+        
+        dp[0] = 1;
+        
+        for(int i=2;i<n+1;i++){
+            if(nums[i-1]==nums[i-2] && dp[i-2])
+                dp[i] = 1;
+            if(i>2 && nums[i-1]==nums[i-2] && nums[i-2]==nums[i-3]&& dp[i-3])
+                dp[i] = 1;
+            if(i>2 && nums[i-1]==1+nums[i-2] && nums[i-2]==1+nums[i-3]&& dp[i-3])
+                dp[i] = 1;
+        }
+        return dp[n];
     }
 };
