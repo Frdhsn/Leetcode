f2e6a3a288fdaad3bda74de991663b9eabf1798b
Md. Farhad Hossain <iamforu299@gmail.com>
2022-08-08 08:51:13 +0600

    Time: 2188 ms (5.00%), Space: 38.1 MB (17.16%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -4,18 +4,15 @@ public:
     int dp[2505][2505];
     int go(int idx,int prv,vector<int>& nums){
         if(idx >= nums.size())return 0;
-        if(dp[idx][prv]!=-1)
-            return dp[idx][prv];
+        if(dp[idx+1][prv+1]!=-1)
+            return dp[idx+1][prv+1];
         int ret = go(idx+1,prv,nums);
-        if(nums[idx]>nums[prv])ret=max(ret,1+go(idx+1,idx,nums));
-        return dp[idx][prv] = ret;
+        if(prv==-1 or nums[idx]>nums[prv])ret=max(ret,1+go(idx+1,idx,nums));
+        return dp[idx+1][prv+1] = ret;
     }
     int lengthOfLIS(vector<int>& nums) {
         memset(dp,-1,sizeof dp);
-        int ret=0;
-        for(int i=0;i<nums.size();i++){
-            ret=max(ret,go(i+1,i,nums)+1);
-        }
+        int ret=go(0,-1,nums);
         return ret;
     }
 };
