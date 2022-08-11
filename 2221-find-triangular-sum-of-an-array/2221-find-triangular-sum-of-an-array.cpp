29e30b47e7ed3f189cb871905f740e8f1563e3cf
Md. Farhad Hossain <iamforu299@gmail.com>
2022-08-12 05:54:42 +0600

    Time: 404 ms (64.41%), Space: 19.4 MB (46.29%) - LeetHub

--- 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.cpp
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     int triangularSum(vector<int>& nums) {
-        int n  = nums.size(),i=0,j=0;
-        vector<int>temp(n);
-        while(j<nums.size()-1){
-            for(int i=0;i<nums.size()-1;i++)
-                temp[i]= (nums[i]+nums[i+1])%10;
-            nums = temp;
-            j++;
+        int n = nums.size();
+        // generating pascals traingle
+        vector<int> triangle(n+1,0);
+        
+        triangle[0] = 1;
+        for(int i=1;i<n;i++){
+            for(int j=i;j>=1;j--){
+                triangle[j]+=triangle[j-1]%10;
+                triangle[j]%=10;
+            }
         }
-        return nums[0];
+        int ret=0;
+        for(int i=0;i<n;i++){
+            ret+=(triangle[i]*nums[i])%10;
+            ret%=10;
+        }
+        cout<<endl;
+        return ret;
     }
 };
