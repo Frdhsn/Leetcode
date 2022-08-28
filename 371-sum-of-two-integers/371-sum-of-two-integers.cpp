a843f0c628d865c1234c7cd45ea9bfe390744c42
Md. Farhad Hossain <iamforu299@gmail.com>
2022-08-29 03:23:32 +0600

    Time: 4 ms (22.47%), Space: 5.9 MB (74.05%) - LeetHub

---
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int add(int x, int y)
+    {
+        // Iterate till there is no carry
+        while (y != 0)
+        {
+            // carry should be unsigned to
+            // deal with -ve numbers
+            // carry now contains common
+            //set bits of x and y
+            unsigned int carry = x & y;
+
+            // Sum of bits of x and y where at
+            //least one of the bits is not set
+            x = x ^ y;
+
+            // Carry is shifted by one so that adding
+            // it to x gives the required sum
+            y = carry << 1;
+        }
+        return x;
+    }
+    int getSum(int a, int b) {
+        return add(a,b);
+    }
+};
