45d1c76a5d09e6754e35522633d3b6fe5de6a649
Md. Farhad Hossain <iamforu299@gmail.com>
2022-08-30 11:10:53 +0600

    Time: 0 ms (100.00%), Space: 6.9 MB (92.76%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -6,20 +6,33 @@
 // bottom right: matrix[n-1-i][n-j-1]
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
+    void transpose(vector<vector<int>>& matrix){
         int n = matrix.size();
-        for(int i=0;i<(n+1)/2;i++){
-            for(int j=0;j<n/2;j++){
-                
-                // bottom left
-                int tmp = matrix[n-1-j][i];
-                
-                matrix[n-1-j][i]=matrix[n-1-i][n-1-j];
-                matrix[n-1-i][n-j-1]=matrix[j][n-1-i];
-                
-                matrix[j][n-1-i]=matrix[i][j];
-                matrix[i][j]=tmp;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
             }
+        }
+    }
+    void print(vector<vector<int>>& matrix){
+        int n = matrix.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                cout<<matrix[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        
+    }
+    void rotate(vector<vector<int>>& matrix) {
+        
+        int n = matrix.size();
+        
+        transpose(matrix);
+        //print(matrix);
+        
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
         }
     }
 };
