bfb8b3bbdfea1cb606c299f77f9610f3967b9205
Md. Farhad Hossain <iamforu299@gmail.com>
2022-10-07 12:18:46 +0600

    Time: 684 ms (6.72%), Space: 38.5 MB (76.54%) - LeetHub

---
+++ 731-my-calendar-ii/731-my-calendar-ii.cpp
@@ -0,0 +1,34 @@
+class MyCalendarTwo {
+public:
+    using ll = long long int;
+    
+    map<ll,int> intervals;
+    MyCalendarTwo() {
+        
+        intervals.clear();
+    }
+    
+    bool book(int start, int end) {
+        intervals[start]++;
+        intervals[end]--;
+        ll sum=0;
+        int res=0;
+        for(auto x:intervals){
+            sum+=x.second;
+            if( sum == 3){
+                
+                intervals[start]--;
+                intervals[end]++;
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
