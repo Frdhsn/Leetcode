2b63de89216c5e0b40d63de58fae9f43a98ce445
Md. Farhad Hossain <iamforu299@gmail.com>
2022-11-28 20:27:45 +0600

    Time: 1109 ms (72.44%), Space: 211.1 MB (8.41%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        map<int,int> st;
+        unordered_set<int> st;
         map<int,int> loss;
         for(auto x:matches){
-            st[x[0]]=1;
-            st[x[1]]=1;
-            if(loss[x[1]] <= 1 )
+            st.insert(x[0]);
+            st.insert(x[1]);
+            if(loss[x[1]] <=1 )
                 loss[x[1]]++;
         }
         vector<vector<int>>res;
         vector<int> f, s;
-        for(auto y:st){
-            auto x = y.first;
+        for(auto x:st){
+            //auto x = y.first;
             if(loss[x]==1){
                 s.push_back(x);
             }
@@ -20,8 +20,8 @@ public:
                 f.push_back(x);
             }
         }
-        // sort(f.begin(),f.end());
-        // sort(s.begin(),s.end());
+        sort(f.begin(),f.end());
+        sort(s.begin(),s.end());
         res.push_back(f);
         res.push_back(s);
         return res;
